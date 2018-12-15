8446dfd7ed6195d68cd415968dd0c124439c2428
Meru <notfood@users.noreply.github.com>
2018-12-15 13:10:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RimWorld-FrameRateControl
+
+[![Version](https://img.shields.io/badge/Rimworld-1.0-green.svg)](http://rimworldgame.com/)
+[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-blue.svg)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+RimWorld mod that disables VSync and allows you to control the max FPS
+
+## License
+Assets are [CC-BY-NC-SA](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+Code is derivate from Ludeon Studio
---
+++ Source/FrameRateControlMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace FrameRateControl
+{
+    [StaticConstructorOnStartup]
+    public class FrameRateControlMod : Mod
+    {
+        public FrameRateControlMod(ModContentPack content) : base(content)
+        {
+            GetSettings<Settings>();
+
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = (int) Settings.targetFrameRate;
+        }
+
+        public override string SettingsCategory() => "Frame Rate Control";
+        public override void DoSettingsWindowContents(Rect inRect) => Settings.DoSettingsWindowContents(inRect);
+    }
+
+    class Settings : ModSettings
+    {
+        public static float targetFrameRate = 60;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref targetFrameRate, "targetFrameRate", 60);
+        }
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard list = new Listing_Standard(GameFont.Small);
+            list.ColumnWidth = inRect.width;
+            list.Begin(inRect);
+            list.Label("Target Frame Rate:" + (Application.targetFrameRate == 0 ? "No Limit" : Application.targetFrameRate.ToString()), -1, "Default 60");
+            targetFrameRate = list.Slider(targetFrameRate, 0f, 300f);
+            list.End();
+
+            Application.targetFrameRate = (int) ((targetFrameRate + 2.5) / 5) * 5;
+        }
+    }
+}
